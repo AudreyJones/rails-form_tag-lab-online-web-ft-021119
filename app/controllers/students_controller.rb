897ad14b7a7473ac2dab2f841c2489a36e9f6f8e
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-22 15:29:32 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,6 +14,7 @@ class StudentsController < ApplicationController
 
   def create
     Student.create(first_name:params[:first_name], last_name:params[:last_name])
+    redirect to students_path
   end
 
 end
